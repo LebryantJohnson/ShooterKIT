c166942be7465a4f134b8d57c4ad4e122a21cf8f
LebryantJohnson <69935607+LebryantJohnson@users.noreply.github.com>
2021-04-01 12:46:06 -0700

    Change Mesh To military Guy

---
+++ Source/SurvivalGameKitV1/SurvivalGameKitV1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SurvivalGameKitV1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalGameKitV1, "SurvivalGameKitV1" );
